7262428fb631f8250b3d1100a2f961b79dd296ec
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-11-15 19:20:03 +0600

    checking if the server running correctly

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ app.get("/", (req, res) => {
   res.send("The Sever is Running!");
 });
 
-app.get("/about", (req, res) => {
-  res.send("This is the Server for the About Page!");
-});
-
-app.get("/about/details", (req, res) => {
-  res.send("This is From the About Details Sever!");
-});
-
 app.listen(port, () => {
   console.log(`This Sever is listeing from port number : ${port}`);
 });
